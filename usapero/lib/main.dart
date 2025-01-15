0991b8560f70a1b6a5e9e0cfc1373bfe1c17bb61
minor_rokafox <rokafox@fastmail.com>
2025-01-15 13:39:01 +0900

    💬 タイトル変更

--- usapero/lib/main.dart
+++ usapero/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ログイン画面',
+      title: 'Disaster Report App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
         useMaterial3: true,
